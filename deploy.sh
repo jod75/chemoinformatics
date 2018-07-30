1811a425c490cb4b8476233d47e49e0773d3939a
jod75 <joseph.demanuele@gmail.com>
2018-07-30 06:54:46 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cd /usr/bin/anaconda/bin/; sudo ./conda install -y -c rdkit rdkit=2016.03.4; sudo ./conda install -y -c sdaxen sdaxen_python_utilities; sudo ./conda install -y -c keiserlab e3fp
+cd /usr/bin/anaconda/bin/
+sudo ./conda install -y -c rdkit rdkit=2016.03.4
+sudo ./conda install -y -c sdaxen sdaxen_python_utilities
+sudo ./conda install -y -c keiserlab e3fp
