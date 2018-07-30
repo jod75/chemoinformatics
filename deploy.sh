32725ca502f50c567b4d12ee293f28e10cee200e
jod75 <joseph.demanuele@gmail.com>
2018-07-30 06:22:05 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /usr/bin/anaconda/bin/; sudo ./conda install -y -c rdkit rdkit=2016.03.4; sudo ./conda install -y -c sdaxen sdaxen_python_utilities; sudo ./conda install -y -c keiserlab e3fp
